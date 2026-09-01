b779f02d4acc822fd3dace9bd1367db54d090ba1
Yotam Faraggi <yotamon@gmail.com>
2026-09-01 12:10:43 +0200

    style: load Stem Intelligence workspace styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Manrope } from "next/font/google";
 import { getSiteUrl } from "@/lib/site-url";
 import { ThemeInitScript } from "@/components/theme-init-script";
 import "./globals.css";
+import "./stem-intelligence.css";
 
 const headingFont = localFont({
   src: "../public/fonts/montage_2/Montage-Demo.ttf",
