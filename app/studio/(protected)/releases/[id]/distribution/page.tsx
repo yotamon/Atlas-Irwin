d544fd79b593b0651eea2d0c9c69d17adaf7de0a
Yotam Faraggi <yotamon@gmail.com>
2026-09-03 00:34:56 +0200

    feat: add release lifecycle controls

--- app/studio/(protected)/releases/[id]/distribution/page.tsx
+++ app/studio/(protected)/releases/[id]/distribution/page.tsx
@@ -1,1 +1,14 @@
-export { default, metadata } from "./release-distribution-page";
+import ReleaseDistributionPage, { metadata } from "./release-distribution-page";
+import ReleaseDistributionLifecycle from "./release-distribution-lifecycle";
+
+export { metadata };
+
+export default async function DistributionRoute(props: {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ notice?: string; error?: string }>;
+}) {
+  return <>
+    <ReleaseDistributionPage {...props} />
+    <ReleaseDistributionLifecycle params={props.params} />
+  </>;
+}
