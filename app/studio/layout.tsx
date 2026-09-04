d6a08f5cf2a9d32cdc552cd497d963e95464bced
Yotam Faraggi <yotamon@gmail.com>
2026-09-04 17:30:22 +0200

    style: load Music workspace polish after UX foundation

--- app/studio/layout.tsx
+++ app/studio/layout.tsx
@@ -19,6 +19,7 @@ import "./ensemblis-screens.css";
 import "./ensemblis-root-isolation.css";
 import "./ensemblis-states.css";
 import "./ux-polish.css";
+import "./music-polish.css";
 
 export const metadata: Metadata = {
   title: {
