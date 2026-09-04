0b72d46d07502652739e53fc384be651d08c5cba
Yotam Faraggi <yotamon@gmail.com>
2026-09-04 17:31:19 +0200

    refactor(sites): remove obsolete preview layout

--- app/studio/site-preview/[siteId]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../../../sites/sites.css";
-
-export const metadata = {
-  robots: { index: false, follow: false },
-};
-
-export default function SitePreviewLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
