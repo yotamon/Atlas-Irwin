f37f8fd5a9f131cf4334ceb28b5e13aceee4e5b2
Yotam Faraggi <yotamon@gmail.com>
2026-09-06 19:04:00 +0200

    refactor: use canonical creative graph module

--- lib/music-intelligence/creative-graph-loader.ts
+++ lib/music-intelligence/creative-graph-loader.ts
@@ -8,7 +8,7 @@ import type { AudioScene, StemDatabase, TrackStem } from "@/types/stem-database"
 import {
   buildTrackCreativeIntelligenceGraph,
   type TrackCreativeIntelligenceGraph,
-} from "./creative-graph-v2";
+} from "./creative-graph";
 
 export async function loadTrackCreativeIntelligenceGraph(
   db: SupabaseClient,
